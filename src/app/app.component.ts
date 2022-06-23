3e35339db4fffb913d35fc7b9df7c8da0cf936a2
gifederspiel <gian.federspiel@bluewin.ch>
2022-06-23 16:53:05 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gianfederspiel.ch';
+
+  toInstagram(){
+    console.log("Test")
+    location.href = "https://www.instagram.com/gian.federspiel/"
+  }
+  toGithub(){
+    location.href = "https://github.com/gifederspiel"
+  }
+  toLinkedIn(){
+    location.href = "https://www.linkedin.com/in/gian-federspiel-469841227/"
+  }
 }
