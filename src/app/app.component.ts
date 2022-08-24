fa34d389aaae1d3f174a09a52f85f4bc90dddc37
Gian Federspiel <69679669+gifederspiel@users.noreply.github.com>
2022-08-24 13:49:09 +0200

    Project slide added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,24 @@ export class AppComponent {
     console.log("Test")
     location.href = "https://www.instagram.com/gian.federspiel/"
   }
-  toGithub(){
-    location.href = "https://github.com/gifederspiel"
+  toMail(){
+    location.href="mailto:gian.federspiel@bluewin.ch?subject=Kontakt"
   }
   toLinkedIn(){
     location.href = "https://www.linkedin.com/in/gian-federspiel-469841227/"
   }
+  toGithub(){
+    location.href = "https://github.com/gifederspiel"
+  }
+  toKapaplaner(){
+    location.href = "https://github.com/PI-Kapazitatsplaner/Kapazitatsplaner"
+  }
+  toGamelauncher(){
+        location.href ="https://github.com/gifederspiel/Fancy-BLJ-Gamelauncher/tree/master"
+  }
+  toGianFederspiel(){
+    location.href ="https://github.com/gifederspiel/gianfederspiel.ch"
+  }
+  picUrl = "assets/profile" + String(Math.floor(Math.random() * 5) + 1) + ".png"
 
 }
