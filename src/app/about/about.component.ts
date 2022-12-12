384365d5e5c53bac7ff84e71c5492524291cc5aa
gifederspiel <gian.federspiel@suva.ch>
2022-12-12 12:41:07 +0000

    Dynamic Age

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,4 +12,13 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  age = calcAge();
+
+}
+function calcAge() {
+  console.log(new Date('2004-06-08'));
+  
+  var timeDiff = Math.abs(Date.now() - +new Date('2004-06-08'));
+  var age = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+  return age;
 }
